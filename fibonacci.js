3f594710e3adc9766638f069c64b9c2555ae6407
Media4tech Srl <info@media4tech.com>
2020-02-21 18:43:23 +0100

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -1,3 +1,13 @@
 /**
 * Scrivere qui il codice della funzione fibonacci(n)
 **/
+var ibonacci = 30;
+
+function straFibonacci(num){ 
+    if (num == 0 || num == 1) {
+        return num;
+    }else{
+        return straFibonacci(num-1) + straFibonacci(num-2); 
+    }
+}
+console.log(straFibonacci(ibonacci));
