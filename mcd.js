1a60fc9b0d14eb6ad6be8946e0f37912ed43697f
Media4tech Srl <info@media4tech.com>
2020-02-21 18:59:20 +0100

    Update mcd.js

--- mcd.js
+++ mcd.js
@@ -1,3 +1,27 @@
 /**
 * Scrivere qui il codice delle funzioni mcd(x, y) e mcdRecursive(x, y)
 **/
+var x = 147;
+var y = 126;
+
+app = 0;
+
+if (y > x) {
+    app = x;
+    x = y;
+    y = app;
+}
+
+
+
+function MCD(a,b) {
+  var r = 0;
+  r=x%y;
+    while(r != 0){
+        a = b;
+        b = r;
+        r = a % b;
+    }
+    return b; 
+}
+console.log(MCD(x,y));
